1d70f3e4aa57cca19ce4e9f7f63b64cd4c260f44
akdimitri <akdimitri@auth.gr>
2019-05-11 00:29:58 +0300

    new code added

---
+++ code/AvrBuild.bat
@@ -0,0 +1,2 @@
+@ECHO OFF
+"C:\Program Files (x86)\Atmel\AVR Tools\AvrAssembler2\avrasm2.exe" -S "C:\Users\dimit\Desktop\silo\labels.tmp" -fI -W+ie -C V2E -o "C:\Users\dimit\Desktop\silo\silo.hex" -d "C:\Users\dimit\Desktop\silo\silo.obj" -e "C:\Users\dimit\Desktop\silo\silo.eep" -m "C:\Users\dimit\Desktop\silo\silo.map" "C:\Users\dimit\Desktop\silo\silo.asm"
